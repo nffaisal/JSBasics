864aad5750497d1bf0de81dbb5a4dae162dc1ec5
Noor <noorffatima0045@gmail.com>
2026-03-30 14:07:37 +0500

    further changes, a button added and shopping cart list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,10 @@
 import Header from './Header.jsx'
 import Footer from'./Footer.jsx'
 import Card from './Card.jsx'
-import Button from './Button.jsx'
 import Student from './Student.jsx'
 import Usergreeting from './Usergreeting.jsx'
+import List from './List.jsx'
+import Button from './Button.jsx'
 function App(){
   return(
     <>
@@ -12,6 +13,7 @@ function App(){
     <Student name="Noor" age={18} isStudent ={true}/>
     <Student/>
     <Card/>
+    <List/>
     <Button/>
     <Footer></Footer>
     </>
--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -7,8 +7,20 @@ function Button(){
    color: "rgb(37, 37, 100)",
    borderRadius: "5px",
     }
+    
+  /*   const handleClick =(name) => {
+        if(count<3){
+            console.log("Thanks for reading!");
+        count++;
+     }else{
+                console.log(`${name} Okay stop clicking`);
+            }
+        }
+     */
+    const handleClick =(e) => e.target.textContent="Thank You!";
+    const handleDoubleClick =(e) =>e.target.textContent ="Thank You Again!";
     return(
-        <button className="button" style={styles}>Click Me!</button>
+        <button style={styles} onClick={handleClick} onDoubleClick={handleDoubleClick}>Click Me!</button>
     );
 }
 export default Button
